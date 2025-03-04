2047e1083c5b587a5ca475318c926fb9949bd3b5
MrLuxan <email.neffworks@googlemail.com>
2025-03-04 23:44:16 +0000

    tooltopstart

--- PetTrainingManual/tooltips.lua
+++ PetTrainingManual/tooltips.lua
@@ -4,9 +4,21 @@ local DB = Addon.DB
 local DCL = Addon.Libs.DCL
 local TameableNPCs = Addon.TameableNPCs
 
-GameTooltip:HookScript("OnTooltipSetUnit", function(self)
-  if not DB.global.npc_tooltips then return end
+local eTooltipShowFor = Addon.eTooltipShowFor
+local eTooltipShowWhen = Addon.eTooltipShowWhen
 
+GameTooltip:HookScript("OnTooltipSetUnit", function(self)
+  --if not DB.global.npc_tooltips then return end
+
+	local _, class = UnitClass("player")
+  if class ~= "HUNTER" and PetTrainingManual.db.tooltipShowFor == eTooltipShowFor.HuntersOnly then
+    return
+  end
+
+  if PetTrainingManual.db.tooltipShowWhen == eTooltipShowWhen.Never then
+    return
+  end
+  
   -- Get unit.
   local _, unit = self:GetUnit()
   if not unit then return end
